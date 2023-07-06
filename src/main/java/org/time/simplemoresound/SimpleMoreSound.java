d430499d8bbf5a14744821f84f78ab85dac944e6
Traveller <2319359689@qq.com>
2023-07-06 11:38:56 +0800

    first commit

---
+++ src/main/java/org/time/simplemoresound/SimpleMoreSound.java
@@ -0,0 +1,17 @@
+package org.time.simplemoresound;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleMoreSound extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
